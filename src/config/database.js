f8a06644bd8799bf3f39ad197960aac2d0c9b66f
brendamatias <brendamatias.sobral@gmail.com>
2020-04-10 17:57:20 -0300

    initial commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+require('dotenv/config');
+
+module.exports = {
+  dialect: 'postgres',
+  url: process.env.DATABASE_URL,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
