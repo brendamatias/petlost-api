32e74d2d51a5d7f7c3ad376ec239470938b3ff19
brendamatias <brendamatias.sobral@gmail.com>
2020-05-18 16:01:39 -0300

    create file route

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 import mongoose from 'mongoose';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -16,7 +17,11 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map((model) => model.init(this.connection));
+    models
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models)
+      );
   }
 
   mongo() {
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+
 import MessageController from './app/controllers/MessageController';
 import KeyController from './app/controllers/KeyController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -16,6 +21,8 @@ routes.use(authMiddleware);
 
 routes.get('/users/:id', UserController.show);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/messages', MessageController.index);
 routes.get('/messages/:id', MessageController.show);
