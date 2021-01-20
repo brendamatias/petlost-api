0a5335b79d7f20e729e83eadc6c5d71e609e4b7f
brendamatias <brendamatias.sobral@gmail.com>
2021-01-20 19:44:56 -0300

    add try catch

--- src/app/mediators/Adresses/Delete.js
+++ src/app/mediators/Adresses/Delete.js
@@ -3,17 +3,21 @@ import responses from '../../../config/httpResponses';
 import BaseException from '../../exceptions/CustomException';
 
 module.exports = async (id, userId) => {
-  const address = await Address.findByPk(id);
+  try {
+    const address = await Address.findByPk(id);
 
-  if (!address) {
-    throw new BaseException('ADDRESS_NOT_FOUND');
+    if (!address) {
+      throw new BaseException('ADDRESS_NOT_FOUND');
+    }
+
+    if (address.user_id !== userId) {
+      throw new BaseException('UNAUTHORIZED_USER');
+    }
+
+    await address.destroy();
+
+    return responses.noContent();
+  } catch (err) {
+    return responses.customError(err);
   }
-
-  if (address.user_id !== userId) {
-    throw new BaseException('UNAUTHORIZED_USER');
-  }
-
-  await address.destroy();
-
-  return responses.noContent();
 };
--- src/app/mediators/Users/Update.js
+++ src/app/mediators/Users/Update.js
@@ -10,63 +10,67 @@ module.exports = async (
   userId,
   { name, email, oldPassword, password, confirmPassword }
 ) => {
-  const schema = Yup.object({
-    name: Yup.string(),
-    email: Yup.string().email(),
-    oldPassword: Yup.string().min(6),
-    password: Yup.string()
-      .min(6)
-      .when('oldPassword', (oldPasswordV, field) =>
-        oldPasswordV ? field.required() : field
+  try {
+    const schema = Yup.object({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPasswordV, field) =>
+          oldPasswordV ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (passwordV, field) =>
+        passwordV ? field.required().oneOf([Yup.ref('password')]) : field
       ),
-    confirmPassword: Yup.string().when('password', (passwordV, field) =>
-      passwordV ? field.required().oneOf([Yup.ref('password')]) : field
-    ),
-  });
-
-  if (
-    !(await schema.isValid({
-      name,
-      email,
-      oldPassword,
-      password,
-      confirmPassword,
-    }))
-  ) {
-    throw new BaseException('VALIDATION_FAILS');
-  }
-
-  let user = await User.findByPk(userId);
-
-  if (!user) {
-    throw new BaseException('USER_NOT_FOUND');
-  }
-
-  if (email !== user.email) {
-    const userExists = await User.findOne({
-      where: { email },
     });
 
-    if (userExists) {
+    if (
+      !(await schema.isValid({
+        name,
+        email,
+        oldPassword,
+        password,
+        confirmPassword,
+      }))
+    ) {
+      throw new BaseException('VALIDATION_FAILS');
+    }
+
+    let user = await User.findByPk(userId);
+
+    if (!user) {
+      throw new BaseException('USER_NOT_FOUND');
+    }
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        throw new BaseException('USER_ALREADY_CREATED');
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
       throw new BaseException('USER_ALREADY_CREATED');
     }
+
+    await user.update({ name, email, password });
+
+    user = await User.findByPk(userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+
+    return responses.ok(user);
+  } catch (err) {
+    return responses.customError(err);
   }
-
-  if (oldPassword && !(await user.checkPassword(oldPassword))) {
-    throw new BaseException('USER_ALREADY_CREATED');
-  }
-
-  await user.update({ name, email, password });
-
-  user = await User.findByPk(userId, {
-    include: [
-      {
-        model: File,
-        as: 'avatar',
-        attributes: ['id', 'path', 'url'],
-      },
-    ],
-  });
-
-  return responses.ok(user);
 };
