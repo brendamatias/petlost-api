2b1db22dc7cf8d6aac9dded93d01483c39939aa1
brendamatias <brendamatias.sobral@gmail.com>
2021-01-21 21:30:19 -0300

    create auth tests

---
+++ __tests__/integration/auth.test.js
@@ -0,0 +1,59 @@
+import request from 'supertest';
+import app from '../../src/app';
+
+import factory from '../factories';
+
+describe('Authentication', () => {
+  let user = null;
+
+  beforeAll(async () => {
+    user = await factory.create('User', { password: '123456' });
+  });
+
+  it('should receive JWT token when authenticated with valid credentials', async () => {
+    const response = await request(app).post('/auth').send({
+      email: user.email,
+      password: '123456',
+    });
+
+    expect(response.status).toBe(201);
+  });
+
+  it('should not authenticated with invalid credentials', async () => {
+    const response = await request(app)
+      .post('/auth')
+      .send({ email: user.email, password: '123123' });
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should return jwt token when authenticated', async () => {
+    const response = await request(app)
+      .post('/auth')
+      .send({ email: user.email, password: '123456' });
+
+    expect(response.body.data).toHaveProperty('token');
+  });
+
+  it('should be able to access private routes when authenticated', async () => {
+    const response = await request(app)
+      .get('/pets')
+      .set('Authorization', `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should not be able to access private routes without jwt token', async () => {
+    const response = await request(app).get('/pets');
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should not be able to access private routes with invalid jwt token', async () => {
+    const response = await request(app)
+      .get('/adresses')
+      .set('Authorization', 'Bearer 123123');
+
+    expect(response.status).toBe(401);
+  });
+});
