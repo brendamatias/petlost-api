727467b48098a6ebcc7e6e0a9d91ed0c7372550c
brendamatias <brendamatias.sobral@gmail.com>
2021-01-21 21:30:40 -0300

    fix when rollback migration

--- src/database/migrations/20200518185259-add-avatar-field-to-users.js
+++ src/database/migrations/20200518185259-add-avatar-field-to-users.js
@@ -13,6 +13,6 @@ module.exports = {
   },
 
   down: (queryInterface) => {
-    queryInterface.removeColumn('users', 'avatar_id');
+    return queryInterface.removeColumn('users', 'avatar_id');
   },
 };
