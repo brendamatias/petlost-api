90c9f7045fe2151e50ac149e8b36a241071af7b0
brendamatias <brendamatias.sobral@gmail.com>
2021-01-28 14:38:49 -0300

    fix: update.test.js

--- __tests__/functional/users/update.test.js
+++ __tests__/functional/users/update.test.js
@@ -28,7 +28,7 @@ describe('Store user', () => {
     const newName = faker.name.findName();
 
     const response = await request(app)
-      .put(`/users/${userOne.id}`)
+      .put('/users')
       .send({ name: newName })
       .set('Authorization', `Bearer ${userOne.generateToken()}`);
 
@@ -38,7 +38,7 @@ describe('Store user', () => {
 
   it('should update the user email successfully', async () => {
     const response = await request(app)
-      .put(`/users/${userOne.id}`)
+      .put('/users')
       .send({ email: 'newemail@teste.com' })
       .set('Authorization', `Bearer ${userOne.generateToken()}`);
 
@@ -46,18 +46,9 @@ describe('Store user', () => {
     expect(response.body.data).toHaveProperty('email', 'newemail@teste.com');
   });
 
-  it('should not update user with invalid id', async () => {
-    const response = await request(app)
-      .put(`/users/${userOne.id}1231231231`)
-      .send({ email: userTwo.email })
-      .set('Authorization', `Bearer ${userOne.generateToken()}`);
-
-    expect(response.status).toBe(401);
-  });
-
   it('should not update the user email with existing a email', async () => {
     const response = await request(app)
-      .put(`/users/${userOne.id}`)
+      .put('/users')
       .send({ email: userTwo.email })
       .set('Authorization', `Bearer ${userOne.generateToken()}`);
 
@@ -66,7 +57,7 @@ describe('Store user', () => {
 
   it('should update the user password successfully', async () => {
     const response = await request(app)
-      .put(`/users/${userOne.id}`)
+      .put('/users')
       .send({
         oldPassword: '123456',
         password: '123123',
@@ -79,7 +70,7 @@ describe('Store user', () => {
 
   it('should not update the user password with a password less than 6 characters', async () => {
     const response = await request(app)
-      .put(`/users/${userTwo.id}`)
+      .put('/users')
       .send({
         oldPassword: '123456',
         password: '123',
@@ -92,7 +83,7 @@ describe('Store user', () => {
 
   it('should not update the user password without oldPassword', async () => {
     const response = await request(app)
-      .put(`/users/${userTwo.id}`)
+      .put('/users')
       .send({
         password: '123',
         confirmPassword: '123',
@@ -104,7 +95,7 @@ describe('Store user', () => {
 
   it('should not update the user password with invalid oldPassword', async () => {
     const response = await request(app)
-      .put(`/users/${userTwo.id}`)
+      .put('/users')
       .send({
         oldPassword: '123456789',
         password: '1234567',
@@ -117,7 +108,7 @@ describe('Store user', () => {
 
   it('should not update the user password without confirmPassword', async () => {
     const response = await request(app)
-      .put(`/users/${userTwo.id}`)
+      .put('/users')
       .send({
         oldPassword: '123456',
         password: '1234567',
@@ -129,7 +120,7 @@ describe('Store user', () => {
 
   it('should not update the user password without password', async () => {
     const response = await request(app)
-      .put(`/users/${userTwo.id}`)
+      .put('/users')
       .send({
         oldPassword: '123456',
         confirmPassword: '1234567',
@@ -141,22 +132,11 @@ describe('Store user', () => {
 
   it('should not update the user password with different password than password confirmation', async () => {
     const response = await request(app)
-      .put(`/users/${userTwo.id}`)
+      .put('/users')
       .send({
         oldPassword: '123456',
         password: '123456789',
         confirmPassword: '1234567',
-      })
-      .set('Authorization', `Bearer ${userTwo.generateToken()}`);
-
-    expect(response.status).toBe(401);
-  });
-
-  it('should not update a user that i dont have access', async () => {
-    const response = await request(app)
-      .put(`/users/${userOne.id}`)
-      .send({
-        name: faker.name.findName(),
       })
       .set('Authorization', `Bearer ${userTwo.generateToken()}`);
 
