dbe093b0d89f2ed1f80598c436985f00b6c3b0ce
brendamatias <brendamatias.sobral@gmail.com>
2021-01-31 14:13:12 -0300

    fix try catch

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -6,15 +6,15 @@ import BaseException from '../exceptions/CustomException';
 import authConfig from '../../config/auth';
 
 export default async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader) {
-    throw new BaseException('TOKEN_NOT_PROVIDED');
-  }
-
-  const [, token] = authHeader.split(' ');
-
   try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      throw new BaseException('TOKEN_NOT_PROVIDED');
+    }
+
+    const [, token] = authHeader.split(' ');
+
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
     const user = await User.findByPk(decoded.id);
