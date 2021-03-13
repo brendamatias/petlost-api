d4bd5e1c3b274930f020b04f1a1a45d3deec646f
brendamatias <brendamatias.sobral@gmail.com>
2021-03-12 22:19:52 -0300

    fix: disable pet

--- src/app/mediators/Pets/Delete.js
+++ src/app/mediators/Pets/Delete.js
@@ -1,6 +1,7 @@
 import CachePet from '../../../lib/CachePet';
 
 import Pet from '../../models/Pet';
+import Chat from '../../models/Chat';
 
 import responses from '../../../config/httpResponses';
 import BaseException from '../../exceptions/CustomException';
@@ -17,7 +18,20 @@ module.exports = async (id, userId) => {
       throw new BaseException('UNAUTHORIZED_USER');
     }
 
-    await pet.destroy();
+    pet.status = false;
+
+    await pet.save();
+
+    const chat = await Chat.findOne({
+      where: {
+        pet_id: id,
+      },
+    });
+
+    if (chat) {
+      chat.active = false;
+      await chat.save();
+    }
 
     await CachePet.invalidatePrefix('pets-list');
 
